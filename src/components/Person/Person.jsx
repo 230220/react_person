295d469ac1bf454ab098256f02ddd108abee81d5
annakalmazan <klavachichikova@gmail.com>
2025-09-18 13:59:22 +0200

    Solution for React task 1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const renderPartner = () => {
+    if (!isMarried) {
+      return <p className="Person__partner">I am not married</p>;
+    }
+
+    return (
+      <p className="Person__partner">
+        {partnerName} is my {sex === 'm' ? 'wife' : 'husband'}
+      </p>
+    );
+  };
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {renderPartner()}
+    </section>
+  );
+};
